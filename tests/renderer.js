2998f8a71b4e09181859690ef8f5ebeb8ab4c3e9
Andr3as <andr3as@me>
2015-08-20 21:29:27 +0200

    Fixes test script to create exports directory if missing

--- tests/renderer.js
+++ tests/renderer.js
@@ -1,4 +1,6 @@
 /* Renderer test */
+var fs = require('fs');
+
 var app = require("./app.js");
 var svg2png = require('svg2png');
 
@@ -7,40 +9,47 @@ Renderer.init(app);
 
 var path = app.basepath + "/tests/exports/";
 
-var png = function(name) {
-	svg2png(path + name + '.svg', path + name + '.png', 0.3, function (err) {
-        if (err) console.log("svg2png", err); return;
-    });
-}
+fs.access(path, fs.R_OK | fs.W_OK, function(err) {
+	if (err) {
+		fs.mkdirSync(path);
+	}
 
-//Value + no loading
-Renderer.settings.display_level = "true";
-for (var i = 0; i <= 100; i++) {
-	var name = Renderer.createName(i, false, false);
-	Renderer.render(i, false, false, path + name + ".svg");
+	var png = function(name) {
+		svg2png(path + name + '.svg', path + name + '.png', 0.3, function (err) {
+	        if (err) console.log("svg2png", err);
+	    });
+	}
 
+	//Value + no loading
+	Renderer.settings.display_level = "true";
+	for (var i = 0; i <= 100; i++) {
+		var name = Renderer.createName(i, false, false);
+		Renderer.render(i, false, false, path + name + ".svg");
+
+		png(name);
+	}
+
+	//Value + loading
+	for (var i = 0; i <= 100; i++) {
+		var name = Renderer.createName(i, true, false);
+		Renderer.render(i, true, false, path + name + ".svg");
+
+		png(name);
+	}
+
+	//no value + loading
+	Renderer.settings.display_level = "false";
+	var name = Renderer.createName(0, true, false);
+	Renderer.render(0, true, false, path + name + ".svg");
 	png(name);
-}
 
-//Value + loading
-for (var i = 0; i <= 100; i++) {
-	var name = Renderer.createName(i, true, false);
-	Renderer.render(i, true, false, path + name + ".svg");
+	//no value + no loading
+	var values = [0,10,40,70];
+	for (var i = 0; i < values.length; i++) {
+		var name = Renderer.createName(values[i], false, false);
+		Renderer.render(values[i], false, false, path + name + ".svg");
 
-	png(name);
-}
+		png(name);
+	}
 
-//no value + loading
-Renderer.settings.display_level = "false";
-var name = Renderer.createName(0, true, false);
-Renderer.render(0, true, false, path + name + ".svg");
-png(name);
-
-//no value + no loading
-var values = [0,10,40,70];
-for (var i = 0; i < values.length; i++) {
-	var name = Renderer.createName(values[i], false, false);
-	Renderer.render(values[i], false, false, path + name + ".svg");
-
-	png(name);
-}
+});
